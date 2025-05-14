44f021eead368afc9f0f90f732bfccd505a72bb6
FrancoJuri <franco.developercontact@gmail.com>
2025-05-14 00:38:34 -0300

    the error that the app was not visible on the web was fixed by modifying the app.json, and some minor changes were made, the next step is to make the google sign in work properly

--- redux/store.js
+++ redux/store.js
@@ -5,6 +5,10 @@ export const store = configureStore({
   reducer: {
     user: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }),
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
 export default store; 
